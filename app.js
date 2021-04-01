9b025cde8dc262fb98602f2a8b2eec9ad70d654c
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2021-04-02 00:01:03 +0200

    update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
