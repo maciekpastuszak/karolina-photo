399e5c53c9871faa344d41d006a49e353734407e
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2021-04-02 01:32:54 +0200

    add a boilerplate and about page

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.get('/', (req, res) => {
     res.render('home');
 })
 
+app.get('/about', (req, res) => {
+    res.render('about');
+})
+
 app.listen(3000, () => {
     console.log("LISTENING ON PORT 3000")
 })
