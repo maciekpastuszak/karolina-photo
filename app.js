8d0adf4dc0caa503ba63b9aa41276778a546d646
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2021-04-01 23:45:59 +0200

    connect to ddatabase

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://ocalhost:27017/karolina-photo', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+})
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
