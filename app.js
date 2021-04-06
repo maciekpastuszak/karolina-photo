ee21625673f1c1098c5983452e169151fdf15915
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2021-04-06 19:11:56 +0200

    update nav and add carousel on homepage

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     res.render('home');
