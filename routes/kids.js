1b12e2c4ffeb10796b92271bfc6c1eba757e2b61
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2021-05-18 09:11:00 +0200

    setup cloudinary upload routes

--- routes/kids.js
+++ routes/kids.js
@@ -5,7 +5,8 @@ const catchAsync = require('../utils/catchAsync');
 const { Kids } = require('../models/photoshoot');
 const {isLoggedIn, isOwner, validateKids} = require('../middleware');
 const multer  = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const {storage} = require('../cloudinary');
+const upload = multer({ storage });
 
 router.route('/')
     .get(catchAsync(kids.index))
