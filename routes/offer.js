1ca56baf50a8f3be2c9df5c14aa2ea69128e9e39
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-03-04 22:15:46 +0100

    bugfix w navbar i routerze

--- routes/offer.js
+++ routes/offer.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const offer = require('../controllers/offer');
 const catchAsync = require('../utils/catchAsync');
 
-router.route('/')
+router.route('/oferta-brzuszkowa')
     .get(catchAsync(offer.tummyOffer))
 
     module.exports = router
