16251e611e61e8f3922c0c185aec380120bd7f86
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-03-04 22:15:20 +0100

    bugfix w kontrolerze offer

--- controllers/offer.js
+++ controllers/offer.js
@@ -1,5 +1,7 @@
+const nonce = require('../utils/nonce');
+
 module.exports.tummyOffer = (req, res) => {
-    res.render('/oferta/oferta-brzuszkowa', {    style: 'offer', 
+    res.render('oferta/oferta-brzuszkowa', {    style: 'offer', 
                                           title:"Fotografia brzuszkowa | Fotograf Bielsko", 
                                           metaDescription: "Sesja brzuszkowa w studio lub w plenerze. Zapraszam do zapoznania się z ofertą. Spraw sobie oraz bliskim Ci osobom pamiątkę na całe życie.",
                                           metaKeywords: "fotograf bielsko, sesja brzuszkowa, sesja ciązowa", 
